8b993965088a5f06e8cd2487c636f0afaa84479d
Alexander Abashkin <monolithed@gmail.com>
2021-01-25 01:42:57 +0300

    Suported prefixCls

--- src/components/pulsar/pulsar.tsx
+++ src/components/pulsar/pulsar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ConfigContext, ConfigConsumerProps} from 'antd/lib/config-provider';
 import {Product} from 'src/config';
 
 import './pulsar.less';
@@ -6,6 +7,9 @@ import './pulsar.less';
 type PulsarProps = {
     color?: string;
     size?: 'large' | 'middle' | 'small' | number;
+    style?: React.CSSProperties;
+    className?: string;
+    prefixCls?: string;
 };
 
 type Size = Record<string, number>;
@@ -23,30 +27,40 @@ const DEFAULT_SIZE = 'small';
 
 const Pulsar: React.FunctionComponent<PulsarProps> = ({
     color = DEFAULT_COLOR,
-    size = DEFAULT_SIZE
+    size = DEFAULT_SIZE,
+    prefixCls,
+    ...props
 }) => {
+    const {getPrefixCls} = React.useContext<ConfigConsumerProps>(ConfigContext);
+    const className = getPrefixCls(Product.PREFIX, prefixCls);
+
     const sizeValue = sizeType[size] || size as number;
     const ripple = sizeValue * 3;
     const position = -(sizeValue * 3) / 2;
 
     return (
         <div data-test={displayName}
-             className={`${Product.PREFIX}-pulsar`}
-             style={{
-                 backgroundColor: color,
-                 height: sizeValue,
-                 width: sizeValue
-             }}
+             aria-live="polite"
+             role="status"
+             {...props}
         >
-            <div className={`${Product.PREFIX}-pulsar__ripple`}
+            <div className={`${className}-pulsar`}
                  style={{
                      backgroundColor: color,
-                     width: ripple,
-                     height: ripple,
-                     marginTop: position,
-                     marginLeft: position
+                     height: sizeValue,
+                     width: sizeValue
                  }}
-            />
+            >
+                <div className={`${className}-pulsar__ripple`}
+                     style={{
+                         backgroundColor: color,
+                         width: ripple,
+                         height: ripple,
+                         marginTop: position,
+                         marginLeft: position
+                     }}
+                />
+            </div>
         </div>
     );
 };
